95472b41e1a9df96e0e7283ff798c2f3e13f43ff
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-02-15 01:25:37 +0000

    Listing records with a specific score range

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,7 @@
+-- Listing all records with a score in the table specified
+-- Results should have both score and name in the order score,name
+
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DES;
