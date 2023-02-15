e9166c4b36f20492dd5ffcf8fbe1dd1890c6d9d2
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-02-15 01:38:14 +0000

    Computing average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- Script to do some computation--
+-- Computing the score average of the records in the table specified
+-- Result column should be average
+
+SELECT AVG(score) AS average 
+FROM second_table;
