bfe4756894d085c9c8a3e6c61b1d72eb9c35a836
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-02-15 01:16:36 +0000

    CReates a second table with 3 row values added

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- Creating a table named second_table in the DB of mysql server
+-- Using some specified columns and data as well
+
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score)
+VALUES (1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
