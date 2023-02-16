139b02f141b8af04d96e886665a19737d74f0cbd
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-02-16 00:09:17 +0000

    Creates a table unique_id with a primary key field too

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,4 @@
+-- creates a table called unique_id on your MySQL server.
+-- unique_id parameters are id INT with the default value 1 and must be unique, name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUE DEFAULT 1, name VARCHAR(256));
