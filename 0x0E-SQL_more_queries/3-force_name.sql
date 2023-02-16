d0607c39ee28d075188fd6f8b508fae80feec454
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-02-16 00:01:56 +0000

    Creates a table and its parameters

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- Script creates the table force_name on your MySQL server.
+-- table description is 'id INT', name VARCHAR(256) Not Null
+
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
