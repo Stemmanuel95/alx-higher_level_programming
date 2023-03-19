8567f17cff06db44fa6da7bf3e6a9305054598b7
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-03-19 21:11:39 +0000

    Script to select all states from database using ORMs in python

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """ Lists all states from database"""
 
-from sys import argv
 import MySQLdb
-if __name == "__main__":
-    username = argv[1]
-    password = argv[2]
-    db_name = argv[3]
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=db_name)
-    cur = db.cursor()
-    cur.execute("SELECT states FROM hbtn_0e_0_usa ORDER BY states.id ASC;")
-    rows = cur.fetchall()
-    
-    for row in rows:
-        print(row)
+from sys import argv
 
-    cur.close()
-    db.close()
+if __name__ == '__main__':
+""" Connecting to the database to get the states recordse."""
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute("SELECT * FROM states")
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
