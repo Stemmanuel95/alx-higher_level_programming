ffdc8b20d249e17a2b0291e89fb37397e99f39c8
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-04-02 22:02:33 +0000

    Script sends a request to the url and display the size of the body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#The purpose of the script below is to take in a url and display the size
-#of the body response
-
+#The purpose of the script below is to take in a url and display the size of the body response
 curl -s1 "$1" | grep "Content-Length:" | cut -d' ' -f2
