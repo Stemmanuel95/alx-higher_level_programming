6e328005dc583c28e6d1d34cfe69abd0254fa0cb
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-04-05 05:16:01 +0000

    Script fetches url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url_link = "https://alx-intranet.hbtn.io/status"
-    request = urllib.request.Request(url_link)
-    with urllib.request.urlopen(request) as response:
+    url_link = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.url_link.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8"))))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
