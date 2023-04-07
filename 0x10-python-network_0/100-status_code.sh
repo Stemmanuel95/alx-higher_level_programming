5f58e6efe0e0ac5675fee5dd79e55a851a14bc0e
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-04-07 15:23:02 +0000

    Script sends a request to a url

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script sends a request to a URL used as argument
-# and show the status code of the response.
+# Script sends a url request, displays only the status code of the response.
 curl -s -o /dev/null -w "%{http_code}" "$1"
