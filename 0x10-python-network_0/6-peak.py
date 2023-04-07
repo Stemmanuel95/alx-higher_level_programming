d9ec47383c6211d4b5137d56eee6e030c92831a8
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-04-07 13:10:31 +0000

    Fixed documentation errors

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,6 +9,7 @@ def find_peak(list_of_integers):
     """
     Returns the peak of the list_of_integers or None.
     """
+    
     length = len(list_of_integers)
     mid_element = length
     mid = length // 2
@@ -18,14 +19,17 @@ def find_peak(list_of_integers):
 
     while True:
         mid_element = mid_element // 2
+        
         if (mid < length - 1 and
                 list_of_integers[mid] < list_of_integers[mid + 1]):
             if mid_element // 2 == 0:
                 mid_element = 2
             mid = mid + (mid_element // 2)
+        
         elif mid_element > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
             if mid_element // 2 == 0:
                 mid_element = 2
             mid = mid - mid_element // 2
+        
         else:
             return list_of_integers[mid]
