746cdc5bb922f4c511f5a895b714cf52ad771ae8
Emmanuel Yeboah Manu <110849861+Stemmanuel95@users.noreply.github.com>
2023-04-29 23:17:55 +0000

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
-for alphabets in range(97,123):
-    if chr(alphabets) in ['q','e']:
+#!/usr/bin/env python3
+
+for alphabet in range(97, 123):
+    if chr(alphabet) in ['q', 'e']:
         continue
     else:
-        print(chr(alphabets),end="")
+        print(chr(alphabet), end="")
