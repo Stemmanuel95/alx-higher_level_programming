52ad409f3244afc6f0fb6d0b3016f7140c1c5843
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-08-15 10:52:38 +0000

    Script prints first argument passed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const [argumentOne, argumentTwo] = process.argv.slice(2);
+const [argumentOne] = process.argv.slice(2);
 
 if (!argumentOne) {
   console.log('No argument');
 } else {
-  console.log('Argument One:', argumentOne);
-  console.log('Argument Two:', argumentTwo);
+  console.log(argumentOne);
 }
