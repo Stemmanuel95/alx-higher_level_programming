4e5a15b6d7858e6ccf668a23ea39c86132080f8e
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-08-21 01:16:05 +0000

    My script lists all states with a name like N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
+"""
+listing all states with
+name starting with `N`
+from the database `hbtn_0e_0_usa`.
+"""
 
 import MySQLdb as db
 from sys import argv
 
 """
-Script connects to the db and lists all states
+Connects to the database and extracts all states
 from the database.
 """
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     db_connection = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db_connection.cursor()
 
     db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' \
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
                 ORDER BY states.id ASC")
 
-    results_rows = db_cursor.fetchall()
+    rows_selected = db_cursor.fetchall()
 
-    for results in results_rows:
+    for row in rows_selected:
         print(row)
