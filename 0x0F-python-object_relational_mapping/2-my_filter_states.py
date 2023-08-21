5bf813620c439082a72e21ac2da33cdaa6846677
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-08-21 01:29:23 +0000

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     COnnects to the db and extract states
     from the database.
     """
-    db_connection = db.connect(host="localhost", port=3306,
+    db_connecton = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db_connection.cursor()
 
