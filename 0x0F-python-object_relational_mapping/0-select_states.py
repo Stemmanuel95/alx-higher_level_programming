23d12eeb960bc48c3b6f19b83ea332ade3fe9aa3
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-08-21 01:07:45 +0000

    My script lists all states from a specified database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     db_cursor = db_connection.cursor()
 
-    db_cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    db_cursor.execute("SELECT * FROM states")
 
     results_rows = db_cursor.fetchall()
 
