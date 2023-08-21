1caec657daa8f9026d717329a9314007c50f7981
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-08-21 01:12:56 +0000

    My script lists all states with a name like N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import MySQLdb 
+from sys import argv
+
+"""
+Script connects to the db and lists all states
+from the database.
+"""
+
+if __name__ == "__main__":
+    db_connection = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+    db_cursor = db_connection.cursor()
+
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' \
+                ORDER BY states.id ASC")
+
+    results_rows = db_cursor.fetchall()
+
+    for results in results_rows:
+        print(row)
